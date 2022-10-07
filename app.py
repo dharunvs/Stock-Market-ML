5fa8ad87e7465a48c2c3b13aa914d686682ab62c
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2022-10-07 12:48:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+
+from scklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+company = "TSLA"
+
+start = dt.datetime(2012, 1,1)
+end = dt.datetime(2022, 1, 1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fittransform(data["Close"].values.reshape(-1, 1))
+
+prediction_days = 60
+
+
